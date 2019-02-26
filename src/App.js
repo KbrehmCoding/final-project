fd3f44acccff54b8bb1abd183a3635910749e144
Krista Brehm <kristabrehm1@gmail.com>
2019-02-26 16:39:53 -0500

    setting up imports for auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Security, SecureRoute, ImplicitCallback } from '@oktaokta-react';
+
+function onAuthRquired({history}) {
+  history.push('/login');
+}
 
 class App extends Component {
   render() {
     return (
-
+      <Security
+      issuer='https://dev-378075.okta.com/oauth2/default'
+                  client_id='0oabhpsmmfYQT5smT356'
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired}>
+        {/* everything inside router needs to be inside this but the router tags go on the outside of it */}
+      </Security>
     );
   }
 }
