3a668ea39b8e7340a88795eb34f01fd915f072aa
Krista Brehm <kristabrehm1@gmail.com>
2019-02-26 16:47:15 -0500

    added import to bring in the login component which I am going to work on now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Security, SecureRoute, ImplicitCallback } from '@oktaokta-react';
+import Login from './components/auth/Login';
 
 function onAuthRquired({history}) {
   history.push('/login');
@@ -15,6 +16,7 @@ class App extends Component {
                       client_id='0oabhpsmmfYQT5smT356'
                       redirect_uri={window.location.origin + '/implicit/callback'}
                       onAuthRequired={onAuthRequired}>
+          <SecureRoute path='/protected' component={Protected} />
           <Route path='/login' render={() => <Login baseUrl='https://dev-378075.okta.com' />} />
           <Route path='/implicit/callback' component={ImplicitCallback} />
         {/* everything inside router needs to be inside this but the router tags go on the outside of it */}
