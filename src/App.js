3ebc145ddc9a85edd5d0994226039f1a714335ec
Krista Brehm <kristabrehm1@gmail.com>
2019-02-26 16:41:24 -0500

    adding the router tabs

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function onAuthRquired({history}) {
 class App extends Component {
   render() {
     return (
-      <Security
-      issuer='https://dev-378075.okta.com/oauth2/default'
-                  client_id='0oabhpsmmfYQT5smT356'
-                  redirect_uri={window.location.origin + '/implicit/callback'}
-                  onAuthRequired={onAuthRequired}>
+      <Router>
+        <Security
+          issuer='https://dev-378075.okta.com/oauth2/default'
+                      client_id='0oabhpsmmfYQT5smT356'
+                      redirect_uri={window.location.origin + '/implicit/callback'}
+                      onAuthRequired={onAuthRequired}>
         {/* everything inside router needs to be inside this but the router tags go on the outside of it */}
-      </Security>
+        </Security>
+      </Router>
     );
   }
 }
