7324ea039de674d52e4679b6150c4d43d7154bd2
Allie Valder <avalder19@gmail.com>
2019-03-07 20:59:46 -0500

    component folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Wrapper from "./components/Wrapper";
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
 
-    );
+  render() {
+    return(
+      <Wrapper>
+        
+      </Wrapper>
+
+    )
   }
 }
 
